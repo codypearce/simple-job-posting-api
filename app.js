c7f0ef46444bafa1125510d74240a60320e7992e
codypearce <outis161@gmail.com>
2018-04-18 03:19:26 -0700

    set up mongo, set up models and controllers

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/simple-job-posting-api");
+
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);

f692b11344ed22ddfebe08f70cacc23fac4be605
codypearce <outis161@gmail.com>
2018-04-19 18:34:54 -0700

    set up requireAuth for protected routes

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
 const Auth = require("./controllers/auth");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = function(app) {
+    app.get("/", requireAuth, (req, res) => res.send("Authorized"));
     app.post("/signup", Auth.signup);
 };

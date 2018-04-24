7118ca97e26999f527b737457f2bdcfa8ef78c39
codypearce <outis161@gmail.com>
2018-04-24 03:54:58 -0700

    add job data

---
+++ data/jobs.js
@@ -0,0 +1,38 @@
+export let jobs = [
+    {
+        title: "Junior Developer",
+        company: "Google",
+        salary: "80,000",
+        location: "Mountain View, CA",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget ullamcorper nisi, ut accumsan massa. Vestibulum sit amet orci risus. Nulla ex metus, feugiat eget diam vel, vehicula imperdiet erat. Cras id sem non nisl condimentum facilisis. Sed bibendum id augue at lacinia. Donec nec orci quis risus rutrum ultrices at sit amet turpis.",
+        tags: ["Front End"]
+    },
+    {
+        title: "Ruby on Rails Dev",
+        company: "Utah Startup",
+        salary: "75,000",
+        location: "Logan, UT",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget ullamcorper nisi, ut accumsan massa. Vestibulum sit amet orci risus. Nulla ex metus, feugiat eget diam vel, vehicula imperdiet erat. Cras id sem non nisl condimentum facilisis. Sed bibendum id augue at lacinia. Donec nec orci quis risus rutrum ultrices at sit amet turpis.",
+        tags: ["Fullstack"]
+    },
+    {
+        title: "Dev Ops Developer",
+        company: "Digital Ocean",
+        salary: "110,000",
+        location: "New York, NY",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget ullamcorper nisi, ut accumsan massa. Vestibulum sit amet orci risus. Nulla ex metus, feugiat eget diam vel, vehicula imperdiet erat. Cras id sem non nisl condimentum facilisis. Sed bibendum id augue at lacinia. Donec nec orci quis risus rutrum ultrices at sit amet turpis.",
+        tags: ["Dev ops", "remote-ok"]
+    },
+    {
+        title: "Unity Developer",
+        company: "Rockstar",
+        salary: "120,000",
+        location: "New York, NY",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget ullamcorper nisi, ut accumsan massa. Vestibulum sit amet orci risus. Nulla ex metus, feugiat eget diam vel, vehicula imperdiet erat. Cras id sem non nisl condimentum facilisis. Sed bibendum id augue at lacinia. Donec nec orci quis risus rutrum ultrices at sit amet turpis.",
+        tags: ["Game dev"]
+    }
+];
